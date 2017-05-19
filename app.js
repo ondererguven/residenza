79169cf6f211fbf60bf7dfa04e65deb4690a6e6b
Enzo Antonino <enzo.antonino23@gmail.com>
2017-05-19 17:48:15 +0200

    added header to the response in order to allow CORS

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
