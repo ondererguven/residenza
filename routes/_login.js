20e8b467ce14e8d2394442733f515a52fef9648d
Onder Erguven <onder.erguven1@gmail.com>
2017-05-19 17:44:51 +0200

    remodel shuttle

--- routes/_login.js
+++ routes/_login.js
@@ -1,68 +1,68 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../models/user')
-var jwt = require("jsonwebtoken");
+// var express = require('express');
+// var router = express.Router();
+// //var User = require('../models/user')
+// var jwt = require("jsonwebtoken");
 
-router.post('/', function(req, res) {
-    User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
-        if (err) {
-            res.json({
-                type: false,
-                data: "Error occured: " + err
-            });
-        } else {
-            if (user) {
-               res.json({
-                    type: true,
-                    data: user,
-                    token: user.token
-                }); 
-            } else {
-                res.json({
-                    type: false,
-                    data: "Incorrect email/password"
-                });    
-            }
-        }
-    });
-});
+// router.post('/', function(req, res) {
+//     User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
+//         if (err) {
+//             res.json({
+//                 type: false,
+//                 data: "Error occured: " + err
+//             });
+//         } else {
+//             if (user) {
+//                res.json({
+//                     type: true,
+//                     data: user,
+//                     token: user.token
+//                 }); 
+//             } else {
+//                 res.json({
+//                     type: false,
+//                     data: "Incorrect email/password"
+//                 });    
+//             }
+//         }
+//     });
+// });
 
-router.post('/signin', function(req, res) {
-    User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
-        if (err) {
-            res.json({
-                type: false,
-                data: "Error occured: " + err
-            });
-        } else {
-            if (user) {
-                res.json({
-                    type: false,
-                    data: "User already exists!"
-                });
-            } else {
-                var userModel = new User();
-                userModel.email = req.body.email;
-                userModel.password = req.body.password;
-                userModel.save(function(err, user) {
-                    user.token = jwt.sign({user: user}, 'process.env.JWT_SECRET');
-                    user.save(function(err, user1) {
-                        res.json({
-                            type: true,
-                            data: user1,
-                            token: user1.token
-                        });
-                    });
-                })
-            }
-        }
-    });
-});
+// router.post('/signin', function(req, res) {
+//     User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
+//         if (err) {
+//             res.json({
+//                 type: false,
+//                 data: "Error occured: " + err
+//             });
+//         } else {
+//             if (user) {
+//                 res.json({
+//                     type: false,
+//                     data: "User already exists!"
+//                 });
+//             } else {
+//                 var userModel = new User();
+//                 userModel.email = req.body.email;
+//                 userModel.password = req.body.password;
+//                 userModel.save(function(err, user) {
+//                     user.token = jwt.sign({user: user}, 'process.env.JWT_SECRET');
+//                     user.save(function(err, user1) {
+//                         res.json({
+//                             type: true,
+//                             data: user1,
+//                             token: user1.token
+//                         });
+//                     });
+//                 })
+//             }
+//         }
+//     });
+// });
 
-router.post('/verify', function(req, res){
-    jwt.verify(req.body.token, 'process.env.JWT_SECRET', function(err, decoded) {
-        res.json({user: decoded.user});
-    });
-});
+// router.post('/verify', function(req, res){
+//     jwt.verify(req.body.token, 'process.env.JWT_SECRET', function(err, decoded) {
+//         res.json({user: decoded.user});
+//     });
+// });
 
-module.exports = router
+// module.exports = router
--- routes/shuttle.js
+++ routes/shuttle.js
@@ -2,15 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var Shuttle = require('../models/shuttle');
-var ShuttleTrip = require('../models/shuttleTrip');
 
 router.get('/', function(req, res, next) {
-    var s = new Shuttle();
-    s.serviceName = "Shuttle";
-    s.isActive = true;
-    s.save(function (err, shuttle) {
-        res.send(shuttle);
-    });
+   
 });
 
 module.exports = router;
