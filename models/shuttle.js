3fa0d49e62b24cbf8dd2c8737deaedb406872ec6
Onder Erguven <onder.erguven1@gmail.com>
2017-05-19 17:44:51 +0200

    remodel shuttle

--- models/shuttle.js
+++ models/shuttle.js
@@ -4,9 +4,40 @@ var Schema = mongoose.Schema;
 var shuttleSchema = new Schema({
     serviceName: String,
     isActive: Boolean,
-    shuttleTrip: {type: Schema.Types.ObjectId, ref: 'ShuttleTrip'}
+    trip: {type: Schema.Types.ObjectId, ref: 'ShuttleTrip'}
 });
-
 var Shuttle = mongoose.model('Shuttle', shuttleSchema);
 
-module.exports = Shuttle;
+
+var shuttleStopSchema = new Schema({
+    coordinate: {lat: Number, lon: Number},
+    time: String,
+    address: String,
+    name: String
+});
+var ShuttleStop = mongoose.model('ShuttleStop', shuttleStationSchema);
+
+
+var shuttleTripSchema = new Schema({
+    currentLocation: {lat: Number, lon: Number},
+    schedule: [{type: Schema.Types.ObjectId, ref: 'Schedule'}],
+    nextStop: Number,
+    date: Date,
+    driver: {type: Schema.Types.ObjectId, ref: 'OAuthUser'},
+    usersBooked: [{type: Schema.Types.ObjectId, ref: 'OAuthUser'}],
+    delay: Boolean
+});
+var ShuttleTrip = mongoose.model('ShuttleTrip', shuttleTripSchema);
+
+
+var shuttleScheduleSchema = new Schema({
+    stops: [[{type: Schema.Types.ObjectId, ref: 'ShuttleStop'}]],
+    identifierCode: String //A for Monday to Friday, B for Saturday and C for Sunday.
+});
+var ShuttleSchedule = mongoose.model('ShuttleSchedule', shuttleScheduleSchema);
+
+
+module.exports.Shuttle = Shuttle;
+module.exports.ShuttleStop = ShuttleStop;
+module.exports.ShuttleTrip = ShuttleTrip;
+module.exports.ShuttleSchedule = ShuttleSchedule;
