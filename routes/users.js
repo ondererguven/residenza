89e822a29acd50e3fbfc05835b2af5e44ed7a2d4
Enzo Antonino <enzo.antonino23@gmail.com>
2017-05-23 17:46:34 +0200

    added route to create the initial admin user

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,18 @@ router.get('/', function(req, res, next) {
   
 });
 
+/*
+router.get('/create-admin-user', function(req, res){
+  var user = new User({
+    username: "bla",
+    password: "bla",
+    role: "admin"
+  });
+
+  user.save().then(function(userSaved){
+    res.status(200).json(userSaved);
+  });
+});
+*/
+
 module.exports = router;
